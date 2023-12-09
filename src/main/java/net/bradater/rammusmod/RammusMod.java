8bafd4d50994755ae5669190617f89e83c5c1623
ali_husseini@hotmail.fr <ali.h.jaguar@gmail.com>
2023-12-09 22:02:48 +0200

    added rammus item

--- src/main/java/net/bradater/rammusmod/RammusMod.java
+++ src/main/java/net/bradater/rammusmod/RammusMod.java
@@ -1,6 +1,8 @@
 package net.bradater.rammusmod;
 
 import com.mojang.logging.LogUtils;
+import net.bradater.rammusmod.item.ModItems;
+import net.minecraft.world.item.CreativeModeTabs;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.BuildCreativeModeTabContentsEvent;
@@ -29,18 +31,18 @@ public class RammusMod
     {
         IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
 
+        ModItems.register(modEventBus);
+
         // Register the commonSetup method for modloading
         modEventBus.addListener(this::commonSetup);
 
-
-
         // Register ourselves for server and other game events we are interested in
         MinecraftForge.EVENT_BUS.register(this);
-
         // Register the item to a creative tab
         modEventBus.addListener(this::addCreative);
 
         // Register our mod's ForgeConfigSpec so that Forge can create and load the config file for us
+        //maybe delete if future errors
         ModLoadingContext.get().registerConfig(ModConfig.Type.COMMON, Config.SPEC);
     }
 
@@ -52,6 +54,10 @@ public class RammusMod
     // Add the example block item to the building blocks tab
     private void addCreative(BuildCreativeModeTabContentsEvent event)
     {
+        //added item to creative mode tab
+        if(event.getTabKey() == CreativeModeTabs.INGREDIENTS) {
+            event.accept(ModItems.RAMMUS);
+        }
 
     }
 
---
+++ src/main/java/net/bradater/rammusmod/item/ModItems.java
@@ -0,0 +1,27 @@
+package net.bradater.rammusmod.item;
+
+
+import net.bradater.rammusmod.RammusMod;
+import net.minecraft.world.item.Item;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
+
+//holds all the items that will be used in mod
+public class ModItems {
+    //DeferredRegister is a long list of whats in <>, in this cames items or type items
+    // then is registered at a certain time when forge loads the items (more complex definition)
+    public static final DeferredRegister<Item> ITEMS =
+            DeferredRegister.create(ForgeRegistries.ITEMS, RammusMod.MOD_ID);
+
+    public static final RegistryObject<Item> RAMMUS = ITEMS.register("rammus",
+            () -> new Item(new Item.Properties()));
+    //also need to register the deferreregister
+    public static void register(IEventBus eventBus) {
+        ITEMS.register(eventBus);
+    }
+
+
+
+}
